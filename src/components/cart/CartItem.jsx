df8b4e8d8e6e1d05319d1e7e8c577ef24f10d7c6
Wafaa <Wafaabahgat>
2024-06-24 02:19:59 +0300

    cartItem

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -19,6 +19,7 @@ const CartItem = () => {
   const handleDelete = (itemId) => {
     dispatch(removeFromCart({ id: itemId }));
   };
+
   const handleImg = (e) => {
     return e?.imageUrl?.includes("http")
       ? e?.imageUrl
@@ -26,28 +27,28 @@ const CartItem = () => {
   };
 
   return (
-    <div className="flex flex-col flex-1  mt-24">
-      <div className="w-[1300px] h-full">
-        <MetaDate ttl="cart - page" disc="The Cart page" />
-        <div onClick={calculateTotal}>
-          <h2 className="font-bold text-3xl text-green-500 mb-6">
+    <div className="flex flex-col mt-24">
+      <MetaDate ttl="cart - page" disc="The Cart page" />
+    
+        <div onClick={calculateTotal} className="flex flex-col px-10 ">
+          <h2 className="mb-16 text-xl font-bold text-green-500 md:text-3xl">
             Shopping Cart
           </h2>
-          <div className="flex flex-col gap-4 justify-between ml-10">
+          <div className="flex flex-col justify-between gap-4">
             {items &&
               items.map((e) => {
                 console.log(e);
                 return (
                   <React.Fragment key={e?.id}>
-                    <div className="flex">
+                    <div className="flex flex-col w-1/2 mx-auto md:flex-row">
                       <img
                         // src={e.imageUrl ? adminImgUrl(e.imageUrl) : "5"}
                         src={e?.imageUrl ? handleImg(e) : "6"}
-                        className="object-contain w-full max-w-[300px] h-[150px] block m-auto rounded-md"
+                        className="object-contain w-[300px] h-[150px] block rounded-md"
                         // alt={e.name}
                       />
 
-                      <div className="w-[500px]">
+                      <div className="w-full mt-2 md:w-1/2 md:mt-0">
                         <p className="text-xl font-bold">{e?.name}</p>
                         <h4 className="m-2">
                           price : <span>{e?.price}$</span>
@@ -56,24 +57,24 @@ const CartItem = () => {
                           Qty : <span>{e?.quantity}</span>
                         </h4>
                         <button
-                          className="bg-red-500 text-white px-2 py-1 rounded mt-2"
+                          className="px-2 py-1 mt-2 text-white bg-red-500 rounded"
                           onClick={() => handleDelete(e?.id)}
                         >
                           Delete
                         </button>
                       </div>
                     </div>
-                    <span className="w-[1300px] h-[1px] bg-slate-300"></span>
+                    <span className="max-w-[1500px] h-[1px] bg-slate-300"></span>
                   </React.Fragment>
                 );
               })}
           </div>
-          <h2 className=" m-4 text-xl text-right">
+          <h2 className="m-4 text-xl text-right ">
             <span className="font-semibold text-green-500">Subtotal: </span>
             {calculateTotal()}$
           </h2>
         </div>
-      </div>
+
     </div>
   );
 };
